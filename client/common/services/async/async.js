adede0d06541dc9e0924395b7492aacc9620de09
Danny Nelson <danielnelsonguitar@gmail.com>
2014-06-10 16:18:28 -0700

    Remove async service

--- client/common/services/async/async.js
+++
@@ -1,9 +0,0 @@
-/**
- * Service for injecting async
- */
-
-angular.module('services.async', [])
-
-.factory('async', function($window) {
-  return $window.async;
-});
--- client/common/services/async/async.unit.js
+++
@@ -1,7 +0,0 @@
-describe('services.async', function() {
-  beforeEach(module('services.async'));
-  
-  it('injects the async library', inject(function(async) {
-    expect(async).toBeTruthy();
-  }));
-});
